d75296a6e9c8f71a8051ea4c8562a1fbcaff97b5
bishavro1 <142042820+bishavro1@users.noreply.github.com>
2023-08-27 01:39:17 +0600

    Update DrawRainbowTestC7.java

--- DrawRainbowTestC7.java
+++ DrawRainbowTestC7.java
@@ -3,51 +3,48 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
-public class DrawRainbowTest
+public class DrawRainbow extends JPanel
 {
-    public static class DrawRainbow extends JPanel
+    final static Color VIOLET = new Color(128, 0, 128);
+    final static Color INDIGO = new Color(75, 0, 130);
+
+    private Color[] colors =
     {
-        final static Color VIOLET = new Color(128, 0, 128);
-        final static Color INDIGO = new Color(75, 0, 130);
+        Color.WHITE, Color.WHITE, VIOLET, INDIGO, Color.BLUE,
+        Color.GREEN,  Color.YELLOW, Color.ORANGE, Color.RED
+    };
 
-        private Color[] colors =
+    public DrawRainbow()
+    {
+        setBackground(Color.WHITE);
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        int radius = 30;
+
+        int centerX = getWidth()/2;
+        int centerY = getHeight();
+
+        for (int counter = colors.length; counter > 0; counter--)
         {
-            Color.WHITE, Color.WHITE, VIOLET, INDIGO, Color.BLUE,
-            Color.GREEN,  Color.YELLOW, Color.ORANGE, Color.RED
-        };
+            g.setColor(colors[counter - 1]);
 
-        public DrawRainbow()
-        {
-            setBackground(Color.WHITE);
-        }
-
-        public void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            int radius = 50;
-
-            int centerX = getWidth();
-            int centerY = getHeight();
-
-            for (int counter = colors.length; counter > 0; counter--)
-            {
-                g.setColor(colors[counter - 1]);
-
-                g.fillArc(centerX - counter * radius,
-                        centerY - counter * radius,
-                        counter * radius * 2, counter * radius * 2, 0, 180);
-            }
+            g.fillArc(centerX - counter * radius,
+                    centerY - counter * radius,
+                    counter * radius * 2, counter * radius * 2, 0, 180);
         }
     }
 
     public static void main(String[] args)
     {
         JFrame frame = new JFrame();
-        DrawRainbow rainbowPanel = new DrawRainbow();
+        DrawRainbow rainbow = new DrawRainbow();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(rainbowPanel); // Changed 'application' to 'frame' and 'panel' to 'rainbowPanel'
-        frame.setSize(1000, 1000);
+        frame.add(rainbow);
+        frame.setSize(800, 450);
         frame.setVisible(true);
     }
 }
